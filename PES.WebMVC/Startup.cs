ae52e91a4d538fe684edc2ce80794219d127d117
Zachary Espinoza <zespinozacode@gmail.com>
2018-11-28 09:36:46 -0500

    Add project files.

---
+++ PES.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PES.WebMVC.Startup))]
+namespace PES.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
